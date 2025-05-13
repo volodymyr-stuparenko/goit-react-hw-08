c8afb2a9174ea86d916c0e148720cfec68c6227e
Volodymyr <StuparenkoVladimir@gmail.com>
2025-05-14 01:56:22 +0300

    fix selectorsContacts

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,9 +1,9 @@
 import { createSelector } from '@reduxjs/toolkit';
 import { selectNameFilter } from '../filters/selectors';
 
-export const selectContacts = (state = state.contacts.items);
-export const selectLoading = (state = state.contacts.isLoading);
-export const selectError = (state = state.contacts.error);
+export const selectContacts = (state) => state.contacts.items;
+export const selectLoading = (state) => state.contacts.isLoading;
+export const selectError = (state) => state.contacts.error;
 
 export const selectFilteredContacts = createSelector(
   [selectContacts, selectNameFilter],
